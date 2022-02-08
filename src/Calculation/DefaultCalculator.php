adc21577f90bc9970cd0ac97d3f4c6ec73e350b5
James Wagoner <jameswagoner@gmail.com>
2022-02-08 13:05:45 -0600

    don't round with fixed discounts maybe??

--- src/Calculation/DefaultCalculator.php
+++ src/Calculation/DefaultCalculator.php
@@ -19,9 +19,17 @@ class DefaultCalculator implements Calculator
 
                 return $cartItem->getDiscountRate();
             case 'tax':
-                return round($cartItem->priceTarget * ($cartItem->taxRate / 100), $decimals);
+                if ($cartItem->getDiscountType() === CartItem::DISCOUNT_FIXED) {
+                    return round($cartItem->priceTarget * ($cartItem->taxRate / 100), $decimals);
+                }
+
+                return $cartItem->priceTarget * ($cartItem->taxRate / 100);
             case 'priceTax':
-                return round($cartItem->priceTarget + $cartItem->tax, $decimals);
+                if ($cartItem->getDiscountType() === CartItem::DISCOUNT_FIXED) {
+                    return round($cartItem->priceTarget + $cartItem->tax, $decimals);
+                }
+
+                return $cartItem->priceTarget + $cartItem->tax;
             case 'discountTotal':
                 if ($cartItem->getDiscountType() === CartItem::DISCOUNT_FIXED) {
                     return $cartItem->discount * $cartItem->qty;
@@ -29,15 +37,35 @@ class DefaultCalculator implements Calculator
 
                 return round($cartItem->discount * $cartItem->qty, $decimals);
             case 'priceTotal':
-                return round($cartItem->price * $cartItem->qty, $decimals);
+                if ($cartItem->getDiscountType() === CartItem::DISCOUNT_FIXED) {
+                    return round($cartItem->price * $cartItem->qty, $decimals);
+                }
+
+                return $cartItem->price * $cartItem->qty;
             case 'subtotal':
-                return max(round($cartItem->priceTotal - $cartItem->discountTotal, $decimals), 0);
+                if ($cartItem->getDiscountType() === CartItem::DISCOUNT_FIXED) {
+                    return max(round($cartItem->priceTotal - $cartItem->discountTotal, $decimals), 0);
+                }
+
+                return max($cartItem->priceTotal - $cartItem->discountTotal, 0);
             case 'priceTarget':
-                return round(($cartItem->priceTotal - $cartItem->discountTotal) / $cartItem->qty, $decimals);
+                if ($cartItem->getDiscountType() === CartItem::DISCOUNT_FIXED) {
+                    return round(($cartItem->priceTotal - $cartItem->discountTotal) / $cartItem->qty, $decimals);
+                }
+
+                return ($cartItem->priceTotal - $cartItem->discountTotal) / $cartItem->qty;
             case 'taxTotal':
-                return round($cartItem->subtotal * ($cartItem->taxRate / 100), $decimals);
+                if ($cartItem->getDiscountType() === CartItem::DISCOUNT_FIXED) {
+                    return round($cartItem->subtotal * ($cartItem->taxRate / 100), $decimals);
+                }
+
+                return $cartItem->subtotal * ($cartItem->taxRate / 100);
             case 'total':
-                return round($cartItem->subtotal + $cartItem->taxTotal, $decimals);
+                if ($cartItem->getDiscountType() === CartItem::DISCOUNT_FIXED) {
+                    return round($cartItem->subtotal + $cartItem->taxTotal, $decimals);
+                }
+
+                return $cartItem->subtotal + $cartItem->taxTotal;
             default:
                 return;
         }
